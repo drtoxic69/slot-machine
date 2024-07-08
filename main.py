c1817c205d2117835c728c25ec8b89ae5051b2b2
Shivakumar <shivakumarjagadish12@gmail.com>
2024-07-09 02:08:34 +0530

    Add func get_bet().

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
 
 def deposit() -> object:
@@ -31,10 +33,33 @@ def get_number_of_lines():
     return lines
 
 
+def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} and ${MAX_BET}.")
+        else:
+            print("Please enter a number!")
+
+    return amount
+
+
 def main():
-    balance = deposit()
+    balance: int = deposit()
     lines = get_number_of_lines()
-    print(balance, lines)
+    while True:
+        bet = get_bet()
+        total_bet = bet*lines
+
+        if total_bet > balance:
+            print(f"You don't have sufficient balance! \nCurrent Balance: ${balance}")
+        else:
+            break
+    print(f"You are betting ${bet} on {lines} lines. Total bet = ${total_bet}")
 
 
 main()
