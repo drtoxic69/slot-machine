aaf544c13c56c484b453002050eba933f1b8d60b
Shivakumar <shivakumarjagadish12@gmail.com>
2024-07-09 18:15:18 +0530

    Add func get_slot_machine_spin()

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
+import random
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    'A': 2,
+    'B': 4,
+    'C': 6,
+    'D': 8
+}
+
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+        columns.append(column)
+
+    return columns
 
 
 def deposit() -> object:
@@ -53,7 +84,7 @@ def main():
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
-        total_bet = bet*lines
+        total_bet = bet * lines
 
         if total_bet > balance:
             print(f"You don't have sufficient balance! \nCurrent Balance: ${balance}")
